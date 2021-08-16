264e7c8e16f5ff245cda58b8174e2a1b20a178b4
Hyun-Ku Lee; cloud IDE <harry9111@naver.com>
2021-08-16 01:54:53 +0000

    ja! Silbern drei!

---
+++ ANSWER/7656/7656.js
@@ -0,0 +1,24 @@
+/*
+ * Einreich 32254913
+ * 
+ * Copyright (C) 2021. Hyun-Ku Lee
+ * 
+ *  Dieses Programm ist freie Software. Sie können es unter den Bedingungen
+ *  der GNU General Public License, wie von der Free Software Foundation
+ *  veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
+ *  der Lizenz oder (nach Ihrer Option) jeder späteren Version.
+ * 
+ *  Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
+ *  von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite
+ *  Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK.
+ *  Details finden Sie in der GNU General Public License.
+ *
+ *  Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
+ *  Programm erhalten haben. Falls nicht, siehe http://www.gnu.org/licenses/.
+ */
+const rg = /What ([^\?\.]*)\?/g;
+
+require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout,
+}).on('line', l=>[...l.matchAll(rg)].map(e => `Forty-two ${e[1]}.`).forEach(e => console.log(e)));
